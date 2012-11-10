b5fe0277035abf51bdc436db54773083890f2dc3
Peter Braden <peterbraden@peterbraden.co.uk>
2012-11-09 17:29:54 -0800

    setting up trackedobject

--- src/CamShift.cc
+++ src/CamShift.cc
@@ -36,6 +36,20 @@ TrackedObject::New(const Arguments &args) {
 
 
 TrackedObject::TrackedObject(cv::Mat image, cv::Rect rect){
+
+ 
+  // Store HSV Hue Image
+  cv::cvtColor(image, hsv, CV_BGR2HSV); // convert to HSV space
+  //mask out-of-range values
+  int vmin = 65, vmax = 256, smin = 55;
+  cv::inRange(hsv,                               //source
+        cv::Scalar(0, smin, MIN(vmin, vmax), 0),  //lower bound
+        cv::Scalar(180, 256, MAX(vmin, vmax) ,0), //upper bound
+        mask);                                    //destination 
+
+  //extract the hue channel, split: src, dest channels
+  int from_to[] = { 0,0,  1,-1,  2,-1,  3,-1};
+  cv::mixChannels(&hsv, 1,  &hue, 1, from_to, 4);
 }
 
 
