8b899d073ad5f9aff550516cab7b9b2764e8b44c
Peter Braden <peterbraden@peterbraden.co.uk>
2012-11-29 13:44:31 -0800

    don't save images in example

--- examples/motion-track.js
+++ examples/motion-track.js
@@ -13,7 +13,7 @@ vid.read(function(mat){
       console.log(">>", x, ":" , rec)
       if (x % 10 == 0){
         m2.rectangle([rec[0], rec[1]], [rec[2], rec[3]])
-        m2.save('./out-motiontrack-' + x + '.jpg')
+ //       m2.save('./out-motiontrack-' + x + '.jpg')
       }
       if (x<100)
         iter();
