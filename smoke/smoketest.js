ca87972037598fb063abc5cf5fef88bac9f10f61
Peter Braden <peterbraden@peterbraden.co.uk>
2013-05-16 19:02:52 -0700

    test it

--- smoke/smoketest.js
+++ smoke/smoketest.js
@@ -1,7 +1,7 @@
 var cv = require('../lib/opencv')
 
 var trainingData = []
-
+/*
 for (var i = 1; i< 41; i++){
   for (var j = 1; j<10; j++){
     trainingData.push([i,"/Users/peterbraden/Downloads/orl_faces/s" + i + "/" + j + ".pgm" ])
@@ -19,4 +19,10 @@ cv.readImage("/Users/peterbraden/Downloads/orl_faces/s6/10.pgm", function(e, im)
 })
 
 
+*/
+cv.readImage("/Users/peterbraden/Desktop/repos/node-opencv/examples/mona.png", function(e, mat){
+  var th = mat.threshold(20, 200);
+  th.save('out.png')
+
+})
 
