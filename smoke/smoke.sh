13488fa94bd6ed31d306b4c2c6cdf3973322ef97
Loren M. Lang <lorenl@north-winds.org>
2013-07-17 22:26:16 -0700

    Smoke test now uses portable file path
    Added a sanity check to ensure smoke.sh is run from the correct folder
    and use a relative path to the mona image so it works on any system.

--- smoke/smoke.sh
+++ smoke/smoke.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ ! -f smoke/smoketest.js ]; then
+	echo "Please run smoke test from the top-level folder of the repository." >&2
+	exit 1
+fi
+
 node-gyp build && echo '-- Compiled OK -- 
 
 ' && node  smoke/smoketest.js && echo '-- Smoke Done, running tests --
