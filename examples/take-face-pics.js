b8ec62e5f8b10908e46e52c765445d8b5f9d4975
Peter Braden <peterbraden@peterbraden.co.uk>
2013-07-26 10:41:13 -0700

    example of roi

--- examples/take-face-pics.js
+++ examples/take-face-pics.js
@@ -6,6 +6,7 @@ var vid = new cv.VideoCapture(0)
 var snap = function(){
   vid.read(function(err, im){
     im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
+
       if (!faces){
         console.log("No Faces")
         return;
@@ -13,14 +14,15 @@ var snap = function(){
       var face = faces[0]
         , ims = im.size()
       
+      var im2 = im.roi(face.x, face.y, face.width, face.height)
+      /*
       im.adjustROI(
            -face.y
          , (face.y + face.height) - ims[0]
          , -face.x
          , (face.x + face.width) - ims[1])
-   
-      console.log("!!", im.locateROI());
-      var im2 = im.clone()
+         */
+      im2.save('out.jpg')
     })
 
 
