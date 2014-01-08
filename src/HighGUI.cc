db3322ebcd8fedd2417b99f64dc9e80efbcb091a
Edgar O Silva <oswaldo.silva@gmail.com>
2014-01-07 20:36:08 -0600

    Fix to use always the first param as timeout if only that one was provided while keeping backwards compatability with 2 params.

--- src/HighGUI.cc
+++ src/HighGUI.cc
@@ -74,10 +74,12 @@ NamedWindow::BlockingWaitKey(const v8::Arguments& args){
 	//SETUP_FUNCTION(NamedWindow)
 	int time = 0;
 
-  if (args.Length() > 0){
+  if (args.Length() > 1){
     time = args[1]->IntegerValue();
   }else{
-    time = args[0]->IntegerValue();
+    if (args.Length() > 0){
+      time = args[0]->IntegerValue();
+    }
   }
 
   int res = cv::waitKey(time);
