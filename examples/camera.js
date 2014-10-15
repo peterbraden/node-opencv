a62456f47bdd0e07220f9dfde547d3366ff162a2
Peter Braden <peterbraden@peterbraden.co.uk>
2014-10-15 20:59:46 +0200

    Don't assume a camera

--- examples/camera.js
+++ examples/camera.js
@@ -1,16 +1,20 @@
 var cv = require('../lib/opencv');
 
-var camera = new cv.VideoCapture(0);
-var window = new cv.NamedWindow('Video', 0)
-/*
-setInterval(function() {
-  camera.read(function(err, im) {
-    if (err) throw err;
-    console.log(im.size())
-    if (im.size()[0] > 0 && im.size()[1] > 0){
-      window.show(im);
-    }
-    window.blockingWaitKey(0, 50);
-  });
-}, 20);
-*/
+try {
+  var camera = new cv.VideoCapture(0);
+  var window = new cv.NamedWindow('Video', 0)
+  /*
+  setInterval(function() {
+    camera.read(function(err, im) {
+      if (err) throw err;
+      console.log(im.size())
+      if (im.size()[0] > 0 && im.size()[1] > 0){
+        window.show(im);
+      }
+      window.blockingWaitKey(0, 50);
+    });
+  }, 20);
+  */
+} catch (e){
+  console.log("Couldn't start camera:", e)
+}
