6ca8a35338ba26b876a3fbb529c594001a3d2043
Dan Schultzer <dan@dreamconception.com>
2016-09-22 01:48:30 -0700

    Fix issues that came up with debug opencv
    OpenCV compiled with debug has a lot of assertions. There was some issues that came up when I ran the tests on that build.

--- examples/test.js
+++ examples/test.js
@@ -1,6 +1,5 @@
 var cv = require('../lib/opencv');
 
-var mat = new cv.Matrix(1, 2, cv.Constants.CV_8U, [1]);
+var mat = new cv.Matrix(1, 2, cv.Constants.CV_8UC3, [1]);
 var row = mat.pixelRow(0);
 console.log("mat: " + row[0] + row[1]);
-
