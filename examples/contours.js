9cca14830934fa775f27b6230e14709252ffd8b9
Salomão Rodrigues <salomao-rodrigues@users.noreply.github.com>
2017-03-05 18:31:28 +0000

    Convert global scope declaration to local var

--- examples/contours.js
+++ examples/contours.js
@@ -19,7 +19,7 @@ cv.readImage('./files/stuff.png', function(err, im) {
   var all = new cv.Matrix(height, width);
 
   im.convertGrayscale();
-  im_canny = im.copy();
+  var im_canny = im.copy();
 
   im_canny.canny(lowThresh, highThresh);
   im_canny.dilate(nIters);
