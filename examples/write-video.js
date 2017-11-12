e65301dbee3fb1e973609ee3c799069f6e248b39
Simon Hailes <btsimonh@googlemail.com>
2017-11-12 16:22:59 +0000

    examples - update write-video.js for async with test of mat.release() before async done

--- examples/write-video.js
+++ examples/write-video.js
@@ -27,3 +27,33 @@ vid.read(function(err, mat) {
 });
 
 
+
+// restart video read
+var vid2 = new cv.VideoCapture(path.join(__dirname, 'files', 'motion.mov'));
+
+var filename2 = './tmp/output-async-'+new Date().getTime()+'.avi';
+var writer2 = null;
+var x = 0;
+
+// do the same write async
+var iter = function () {
+  vid2.read(function (err, m2) {
+	if (writer2 === null)
+	  writer2 = new cv.VideoWriter(filename2, 'DIVX', vid2.getFPS(), m2.size(), true);
+
+	x++;
+	writer2.write(m2, function(err){
+	  if (x < 100) {
+		iter();
+	  } else {
+		vid2.release();
+		writer2.release();
+	  }
+	});
+	m2.release();
+	delete m2;
+  });
+};
+
+// kick it off
+iter();
